e3d2cf1d3a61f8fb88dea467cfae6da0d99da558
Bryanna Vinge <bevinge711@gmail.com>
2021-10-14 16:48:33 -0500

    working on delete api call

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -1,33 +1,33 @@
-const {uuid} = require('uuidv4');
+const { uuid } = require('uuidv4');
 const notes = require('express').Router();
-const {readFromFile, readAndAppend} = require('../helper/fsUtils');
+const { readFromFile, readAndAppend, writeToFile } = require('../helper/fsUtils');
 
 
-notes.get('/', (req,res)=> {
+notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 })
 
 
-notes.get('/:note_id', (req,res)=>{
-    const noteId =req.params.note_id;
+notes.get('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
     readFromFile('./db/db.json')
-    .then((data)=> JSON.parse(data))
-    .then((json)=>{
-        const results = json.filter((note) => note.note_id === noteId);
-        return results.length > 0
-        ? res.json(results) : res.json("No note with this ID");
-        
-    });
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const results = json.filter((note) => note.note_id === noteId);
+            return results.length > 0
+                ? res.json(results) : res.json("No note with this ID");
+
+        });
 });
 
 notes.post('/', (req, res) => {
     console.log(req.body);
 
-    const {title, text} = req.body;
+    const { title, text } = req.body;
 
-    if(req.body) {
+    if (req.body) {
         const newNote = {
-            title, 
+            title,
             text,
             note_id: uuid(),
         };
@@ -35,11 +35,22 @@ notes.post('/', (req, res) => {
         readAndAppend(newNote, './db/db.json');
         res.json('New note successfully added!');
     }
-    else{
+    else {
         console.log('Error - note not added!')
     }
 })
 
+notes.delete('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const newResult = json.filter((note) => note.note_id !== noteId);
+            writeToFile('./db/db.json', newResult);
+            return newResult
+                ? res.json('Note sucessfully deleted!') : res.json('Note did not delete.')
+        });
+});
 
 module.exports = notes
 
